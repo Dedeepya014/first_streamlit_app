33cef0bdc4874c09db60432bada5bc4e705330b2
Dedeepya014 <151805471+Dedeepya014@users.noreply.github.com>
2023-11-23 10:29:00 -0500

    Update streamlit_app.py
    fixed name errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 
 import streamlit
 streamlit.title('My Parents NEW HEALTHY DINER')
+
 streamlit.header('Breakfast Menu')
-Streamlit.text('Dosa')
-Streamlit.text('Idly')
+streamlit.text('Dosa')
+streamlit.text('Idly')
 streamlit.text('Upma')
