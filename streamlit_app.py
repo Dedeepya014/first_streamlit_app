dcf5b046183c9d010bde2cac78933a8b6dd712ef
Dedeepya014 <151805471+Dedeepya014@users.noreply.github.com>
2023-11-23 10:31:00 -0500

    Update streamlit_app.py
    changed menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents NEW HEALTHY DINER')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Dosa')
-streamlit.text('Idly')
-streamlit.text('Upma')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
